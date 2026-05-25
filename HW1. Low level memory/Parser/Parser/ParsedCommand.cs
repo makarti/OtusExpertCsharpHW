281121d2d83d367c02c65351be53934fcd214c82
Makvetsyan Artur <makvetsyan.arti@gmail.com>
2026-05-26 00:22:23 +0300

    HW 1

---
+++ HW1. Low level memory/Parser/Parser/ParsedCommand.cs
@@ -0,0 +1,12 @@
+namespace Parser;
+
+public readonly ref struct ParsedCommand
+{
+    public ReadOnlySpan<byte> Command { get; init; }
+
+    public ReadOnlySpan<byte> Key { get; init; }
+
+    public ReadOnlySpan<byte> Value { get; init; }
+
+    public bool IsDefault => Command.IsEmpty && Key.IsEmpty && Value.IsEmpty;
+}
