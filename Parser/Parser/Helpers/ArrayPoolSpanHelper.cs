01655cebe37ac050b318b5ae778caad380e9d5b7
Makvetsyan Artur <makvetsyan.arti@gmail.com>
2026-08-21 10:53:33 +0300

    доработка на сервере tcp

---
+++ Parser/Parser/Helpers/ArrayPoolSpanHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parser.Helpers
+{
+    public ref struct ArrayPoolSpanHelper<T> // : IDisposable
+    {
+        private T[] _data;
+        private int _size;
+        public Span<T> SpanScoped => _data.AsSpan(0, _size);
+
+        public ArrayPoolSpanHelper(int size)
+        {
+            _data = ArrayPool<T>.Shared.Rent(size);
+            _size = size;
+        }
+
+        public void Dispose()
+        {
+            _size = 0;
+            ArrayPool<T>.Shared.Return(_data, true);
+        }
+    }
+}
--- Parser/Parser/Servers/TcpServer.cs
+++ Parser/Parser/Servers/TcpServer.cs
@@ -207,7 +207,10 @@ public sealed class TcpServer : IDisposable
                     if (profile is null)
                         return ResponseNil;
 
-                    byte[] json = JsonSerializer.SerializeToUtf8Bytes(profile);
+                    using var memoryStream = new MemoryStream();
+                    profile.SerializeToBinary(memoryStream);
+                    byte[] json = memoryStream.ToArray();
+
                     byte[] response = new byte[json.Length + CrLf.Length];
                     json.CopyTo(response, 0);
                     CrLf.CopyTo(response, json.Length);
